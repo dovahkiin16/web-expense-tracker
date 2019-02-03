b5960d3a88c7432c63540c9f531028db35ce90ec
Eden E. Fernandez Jr <edenfernandezjr@gmail.com>
2019-02-04 03:25:24 +0800

    Add transaction form dialog

---
+++ src/components/Transaction/TransactionFormDialog.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-dialog(max-width="300" :value="open" @input="close")
+    TransactionFormCard(@add="close")
+</template>
+
+<script>
+import TransactionFormCard from "./TransactionFormCard";
+
+export default {
+  name: "TransactionFormDialog",
+  props: ['open'],
+  components: { TransactionFormCard },
+  methods: {
+    close: function () {
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
