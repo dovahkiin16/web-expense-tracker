a8dbd7d2cd0f32e11db871841f97393ed40ce1cd
Eden E. Fernandez Jr <edenfernandezjr@gmail.com>
2019-02-16 00:23:34 +0800

    Add login persistence

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,20 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import transactionModule from './transactions';
 import accountModule from './account';
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+  modules: ['account']
+});
 
 export default new Vuex.Store({
   modules: {
     transaction: transactionModule,
     account: accountModule,
   },
+  plugins: [vuexLocalStorage.plugin]
 });
