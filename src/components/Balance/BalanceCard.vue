457fca8838a22fa9c207f0e672810de0483c88a8
Eden E. Fernandez Jr <edenfernandezjr@gmail.com>
2019-02-24 04:42:59 +0800

    Add balance card

---
+++ src/components/Balance/BalanceCard.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-card
+    v-card-title Balance in PHP
+    v-divider
+    v-card-text.display-3.text-xs-center {{ balance }}
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'BalanceCard',
+  computed: {
+    ...mapGetters('transaction', ['balance'])
+  }
+}
+</script>
