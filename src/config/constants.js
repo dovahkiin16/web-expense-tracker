c02ffb713ee1505c359ec10d718e139235cd6587
Eden E. Fernandez Jr <edenfernandezjr@gmail.com>
2019-02-26 19:08:53 +0800

    Add constants file

---
+++ src/config/constants.js
@@ -0,0 +1,17 @@
+export const categories = {
+  TRANSPORT: { name: 'Transport', type: 'credit' },
+  FOOD: { name: 'Food', type: 'credit' },
+  ENTERTAINMENT: { name: 'Entertainment', type: 'credit' },
+  EXTRA: { name: 'Extra', type: 'credit' },
+  LOAN: { name: 'Loan', type: 'debit' },
+  INCOME: { name: 'Income', type: 'debit' },
+};
+
+export const categories_array = [
+  categories.TRANSPORT,
+  categories.FOOD,
+  categories.LOAN,
+  categories.ENTERTAINMENT,
+  categories.EXTRA,
+  categories.INCOME,
+];
