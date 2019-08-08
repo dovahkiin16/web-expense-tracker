f8047516681a0b6d65ff4780da934cd7f1b13cbb
dovahkiin16 <edenfernandezjr@gmail.com>
2019-08-09 03:43:04 +0800

    add balance in home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
-    
+  v-container(:class="{'pa-0': isDeviceSmall}")
+    v-layout(row wrap)
+      v-flex(xs12 sm6 md4 lg3 )
+        BalanceCard
 </template>
 
 <script>
-export default {
+import BalanceCard from "../components/Dashboard/BalanceCard";
 
-}
+export default {
+  name: "HomeView",
+  components: { BalanceCard },
+  computed: {
+    isDeviceSmall() {
+      return this.$vuetify.breakpoint.xsOnly;
+    }
+  }
+};
 </script>
