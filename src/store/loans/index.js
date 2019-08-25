1838a85f02d97c79a754d1d6bab3bd28d94ec0d1
dovahkiin16 <edenfernandezjr@gmail.com>
2019-08-26 03:24:51 +0800

    add new loan function and new loan dialog

--- src/store/loans/index.js
+++ src/store/loans/index.js
@@ -1,7 +1,12 @@
+import { categories } from '../../config/constants';
 import { db } from '../../main';
 
 const defaultState = {
   loaners: [],
+  loanForm: {
+    amount: 0,
+    name: ''
+  },
 };
 
 const getters = {
@@ -25,11 +30,59 @@ const actions = {
 
     commit('setLoaners', loaners);
   },
+
+  async addLoan({state, commit, dispatch, rootGetters}) {
+    const loan = state.loanForm;
+    const transaction = {
+      amount: loan.amount,
+      category: categories.LOAN.name,
+      type: categories.LOAN.type,
+    };
+
+    // set transaction form fields
+    Object.keys(transaction).forEach(key => {
+      const detail = {
+        field: key,
+        value: transaction[key]
+      };
+
+      commit('transaction/setTransactionFormField', detail, {root: true});
+    })
+
+    // addTransaction and clear form
+    await dispatch('transaction/addTransaction', null, {root: true});
+    commit('transaction/clearTransactionForm', null, {root: true});
+
+    const loansCollection = await db.collection('user')
+      .doc(rootGetters['account/userId'])
+      .collection('loaners');
+    
+    const loanersCollection = await loansCollection.where('name', '==', loan.name).get();
+
+    if (loanersCollection.empty) {
+      loansCollection.add(loan);
+    } else {
+      const oldAmount = loanersCollection.docs[0].get('amount');
+      const newAmount = parseInt(oldAmount) + parseInt(loan.amount);
+
+      await loanersCollection.docs[0].ref.update({amount: newAmount});
+    }
+
+    dispatch('fetchLoaners');
+  },
 };
 
 const mutations = {
   setLoaners(state, data) {
     state.loaners = data;
+  },
+
+  setLoanFormField(state, detail) {
+    state.loanForm[detail.field] = detail.value;
+  },
+
+  clearLoanForm(state) {
+    state.loanForm = defaultState;
   }
 };
 
--- src/views/LoansView.vue
+++ src/views/LoansView.vue
@@ -1,21 +1,45 @@
 <template lang="pug">
-  v-container(:class="{'pa-0': isDeviceSmall}")
-    v-layout(row wrap)
-      v-flex(xs12)
-        loaners-list
+  div
+    v-container(:class="{'pa-0': isDeviceSmall}")
+      v-layout(row wrap)
+        v-flex(xs12)
+          loaners-list
+    v-btn(fab dark fixed bottom right color="primary" @click="openDialog")
+      v-icon add
+    add-loan-dialog(:open="isDialogOpen" @close="closeDialog")
 </template>
 
 <script>
-import LoanersList from "../components/Loan/LoanersList";
+import LoanersList from '../components/Loan/LoanersList';
+import AddLoanDialog from '../components/Loan/AddLoanDialog';
 
 export default {
-  name: "LoansView",
+  name: 'LoansView',
 
-  components: { LoanersList },
+  components: {
+    LoanersList,
+    AddLoanDialog,
+  },
+
+  data() {
+    return {
+      isDialogOpen: false,
+    }
+  },
 
   computed: {
     isDeviceSmall() {
       return this.$vuetify.breakpoint.xsOnly;
+    }
+  },
+
+  methods: {
+    openDialog() {
+      this.isDialogOpen = true;
+    },
+
+    closeDialog() {
+      this.isDialogOpen = false;
     }
   }
 }
